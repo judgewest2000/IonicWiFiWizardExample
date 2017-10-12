b1c9385c3d6ebc102ae514bd0c57ba9acf1a77f0
James West <james.west@westdarley.co.uk>
2017-10-12 23:34:17 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var WifiWizard: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +12,22 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  errorHandler(err: any) {
+    alert(`Problem: ${err}`);
+  }
+
+  getSsidName() {
+    WifiWizard.getCurrentSSID((ssid: string) => alert(`Your SSID: ${ssid}`), this.errorHandler);
+  }
+
+  isWifiEnabled() {
+    WifiWizard.isWifiEnabled(truthy => alert(`Wifi Enabled: ${truthy}`), this.errorHandler);
+  }
+
+  listNetworks() {
+    WifiWizard.listNetworks(networks => alert(`Networks: ${networks}`), this.errorHandler);
+  }
+
 
 }
